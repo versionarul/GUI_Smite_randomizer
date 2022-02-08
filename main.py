7e5b6679a1083eebf9d39371457f372e61bc7c11
versionarul <dinu.alin.alexandru@gmail.com>
2022-02-09 00:02:23 +0200

    Check if json file exists

--- main.py
+++ main.py
@@ -5,50 +5,59 @@ from random import randint
 import json
 
 
+# -------------------- Import JSON --------------------- #
 
-# -------------------- Buttons Functionality --------------------- #
-def clicked_button_random_god():
-    pass
-
-def clicked_button_random_skin():
-    pass
-
-# -------------------- UI Setup --------------------- #
-
-window = tk.Tk()
-window.title('Smite randomizer')
-window.config(padx=35, pady=35)
-
-# creating tkinter var for retaining dropdown menu options
-tkvar = StringVar(window)
-
-# dropdown menu list of choices and initial value
-hero_class = {'Any', 'Assassin', 'Guardian', 'Hunter', 'Mage', 'Warrior'}
-tkvar.set('Choose class')
-
-# dropdown menu
-dropdown_menu = tk.OptionMenu(window, tkvar, *hero_class)
-dropdown_menu.grid(row=2, column=1)
-dropdown_menu.config(width=13)
-
-# entry init + options
-entry_type_god_name = tk.Entry(width=18)
-entry_type_god_name.grid(row=2, column=2)
-entry_type_god_name.insert(0, 'God name or nick')
+try:
+    with open('gods.json', 'r') as json_file:
+        data = json.load(json_file)
 
 
-# buttons init
-button_random_god = tk.Button(text='Random god', command=clicked_button_random_god, width=15)
-button_random_skin = tk.Button(text='Random skin', command=clicked_button_random_skin, width=15)
+    # -------------------- Buttons Functionality --------------------- #
+    def clicked_button_random_god():
+        pass
 
-# buttons grid position
-button_random_god.grid(row=3, column=1)
-button_random_skin.grid(row=3, column=2)
+    def clicked_button_random_skin():
+        pass
 
-# result label init and options
-label_show_results = tk.Label(text='\n\n**********\n\n**********')
-label_show_results.grid(row=5, column=1, columnspan=2, rowspan=4)
+    # -------------------- UI Setup --------------------- #
+
+    window = tk.Tk()
+    window.title('Smite randomizer')
+    window.config(padx=35, pady=35)
+
+    # creating tkinter var for retaining dropdown menu options
+    tkvar = StringVar(window)
+
+    # dropdown menu list of choices and initial value
+    hero_class = {'Any', 'Assassin', 'Guardian', 'Hunter', 'Mage', 'Warrior'}
+    tkvar.set('Choose class')
+
+    # dropdown menu
+    dropdown_menu = tk.OptionMenu(window, tkvar, *hero_class)
+    dropdown_menu.grid(row=2, column=1)
+    dropdown_menu.config(width=13)
+
+    # entry init + options
+    entry_type_god_name = tk.Entry(width=18)
+    entry_type_god_name.grid(row=2, column=2)
+    entry_type_god_name.insert(0, 'God name or nick')
+
+
+    # buttons init
+    button_random_god = tk.Button(text='Random god', command=clicked_button_random_god, width=15)
+    button_random_skin = tk.Button(text='Random skin', command=clicked_button_random_skin, width=15)
+
+    # buttons grid position
+    button_random_god.grid(row=3, column=1)
+    button_random_skin.grid(row=3, column=2)
+
+    # result label init and options
+    label_show_results = tk.Label(text='\n\n**********\n\n**********')
+    label_show_results.grid(row=5, column=1, columnspan=2, rowspan=4)
 
 
 
-tk.mainloop()
+    tk.mainloop()
+
+except FileNotFoundError:
+    messagebox.showinfo(title = 'File not found!', message = 'File gods.json not found. Please make sure you have the file in the same folder and restart the program!')
